376d1a6fb569b5e53607b5c33e391cb10e178e81
yangboz <zhou.yangbo@hp.com>
2014-04-21 13:12:17 +0800

    ADDED:phpunit test case file for testing

---
+++ test/HomeHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+require "/../includes/Database.php";
+
+class HomeHandlerTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testGet()
+    {
+    	$db = Database::Instance();
+		$pdo = $db::$pdo;
+		//
+		$pdo -> beginTransaction();
+	
+		$sth = $pdo -> prepare('SELECT * FROM articles');
+	
+		$sth -> execute();
+	
+		$articles = $sth -> fetchAll(PDO::FETCH_OBJ);
+		// printf(count($articles));
+		$count = count($articles);
+        // Assert
+        $this->assertEquals(0, $count);
+    }
+
+    // ...
+}
